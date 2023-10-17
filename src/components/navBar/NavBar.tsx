ab9e3e1e4ab46aab198eeff6dd349b5e4502c2ad
unknown <guilherme.neto2001@outlook.com>
2023-10-17 18:39:09 -0300

    finalização blogpessoal react, deploy final

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
+function Navbar() {
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent  //variavel usada no conteúdo sobre renderização condicional
+  
+  if(usuario.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+          <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+return (
+  <>
+    {navbarComponent}
+  </>
+)
+}
+
+export default Navbar
